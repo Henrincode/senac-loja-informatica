1bcc50cc0d326e3e97ac490fff25eaaa5fa305b1
Henrincode <henrincode@gmail.com>
2026-02-11 22:28:52 -0300

    feat: creating POST routes in index.js

--- index.js
+++ index.js
@@ -1,47 +1,22 @@
-const mysql = require('mysql')
-const express = require('express')
+import express from 'express'
+import { createConnection } from 'mysql'
+import cors from 'cors'
+
+
 const app = express()
 
+app.use(cors())
+app.use(express.json())
 
-app.get('/', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.send ('ZecaInfo')
-})
-
-// const lista_produtos = [
-//     {
-//         "titulo": "Red Nike",
-//         "foto":"https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxzaG9lfGVufDB8MHx8fDE3MjEwNDEzNjd8MA&ixlib=rb-4.0.3&q=80&w=1080",
-//         "descricao": "Tênis leve, com design versátil e acabamento moderno, perfeito para acompanhar sua rotina.",
-//         "preco": 499.00,
-//         "avaliacao": 5
-//     },
-//     {
-//         "titulo": "Blue Nike",
-//         "foto":"https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//         "descricao": "Modelo confortável, resistente e ideal para quem busca um visual urbano sem abrir mão do bem-estar.",
-//         "preco": 699.00,
-//         "avaliacao": 3
-//     },
-//     {
-//         "titulo": "Black Nike",
-//         "foto":"https://images.unsplash.com/photo-1643584549066-fc993fc9cb43?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//         "descricao": "Tênis com ajuste confortável, visual clean e solado que garante estabilidade em cada passo.",
-//         "preco": 799.00,
-//         "avaliacao": 4
-//     }
-// ]
-
-
-const conexao = mysql.createConnection({
+const conexao = createConnection({
     host: '108.179.193.209',
     user: 'gutoxa27_alunos',
     password: 'JD_eXLNHp1ZG',
     database: 'gutoxa27_bd_loja'
 })
 
-conexao.connect((erro) =>  {
-    if(erro){
+conexao.connect((erro) => {
+    if (erro) {
         console.log('Deu ruim na conexão')
         throw erro
     } else {
@@ -49,9 +24,14 @@ conexao.connect((erro) =>  {
     }
 })
 
-// get all products
-app.get("/produtos", function (req, res){
+// Hello Word
+app.get('/', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
+    res.send('ZecaInfo')
+})
+
+// get all products
+app.get("/produtos", function (req, res) {
     const ordem = req.query.ordem
     conexao.query(`SELECT * FROM produtos ${ordem && `order by ${ordem}`}`, ((erro, lista_produtos, campos) => {
         // console.log(lista_produtos)
@@ -59,55 +39,53 @@ app.get("/produtos", function (req, res){
     }))
 })
 
-// get all products witch filter
-// app.get("/produtos/:ordem", function (req, res){
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     const ordem = req.params.ordem
-//     conexao.query(`SELECT * FROM produtos order by ${ordem}`, ((erro, lista_produtos, campos) => {
-//         // console.log(lista_produtos)
-//         res.send(lista_produtos)
-//     }))
-// })
+// pegar lista de categorias
+app.get("/lista/categorias", function (req, res) {
+    conexao.query(`SELECT distinct categoria FROM produtos`, ((erro, lista_categorias, campos) => {
+        res.send(lista_categorias)
+    }))
+})
 
 // filtrar categorias
-app.get("/produtos/:categoria/", function (req, res){
-    res.setHeader('Access-Control-Allow-Origin', '*')
+app.get("/produtos/:categoria/", function (req, res) {
     const categoria = req.params.categoria
     conexao.query(`SELECT * FROM produtos where categoria = '${categoria}'`, ((erro, lista_produtos, campos) => {
-        // console.log(lista_produtos)
         res.send(lista_produtos)
     }))
 })
 
 // filtrar com ordem
-app.get("/produtos/:categoria/:ordem", function (req, res){
-    res.setHeader('Access-Control-Allow-Origin', '*')
+app.get("/produtos/:categoria/:ordem", function (req, res) {
     const categoria = req.params.categoria
     const ordem = req.params.ordem
-    console.log(ordem)
     conexao.query(`SELECT * FROM produtos where categoria = '${categoria}' order by ${ordem}`, ((erro, lista_produtos, campos) => {
-        // console.log(lista_produtos)
         res.send(lista_produtos)
-    }))
-})
-
-// pegar lista de categorias
-app.get("/lista/categorias", function (req, res){
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    conexao.query(`SELECT distinct categoria FROM produtos`, ((erro, lista_categorias, campos) => {
-        // console.log(lista_categorias)
-        res.send(lista_categorias)
     }))
 })
 
 // get units
-app.get("/unidades", function (req, res){
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // res.send(lista_produtos)
+app.get("/unidades", function (req, res) {
     conexao.query("SELECT * FROM unidades", ((erro, lista_produtos, campos) => {
-        // console.log(lista_produtos)
         res.send(lista_produtos)
     }))
 })
 
-app.listen (3000)
+// creat product
+app.post("/produto/", function (req, res) {
+
+    const { titulo, preco, descricao, avaliacao, foto, categoria } = req.body;
+
+    conexao.query(`
+        INSERT INTO produtos(titulo, foto, descricao, preco, avaliacao, categoria)
+        values('${titulo}','${foto}','${descricao}',${preco}, ${avaliacao}, '${categoria}')`,
+        function (erro, resultado) {
+            if (erro) {
+                res.json(erro);
+            }
+
+            res.send(resultado.insertId);
+        });
+})
+
+
+app.listen(3000)
